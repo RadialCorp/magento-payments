796e8c7dbe158341c445199fac8d5245bc4c39bd
Robert Krule <rkrule@radial.com>
2016-06-26 16:59:55 -0400

    MPTF-128 added extra delivery status column to credit memo and settlement retry, added extra system configurations to form a queue for retry, paginated cron job for creditmemo and settlement calls

---
+++ src/app/code/community/Radial/Payments/Model/Adminhtml/System/Config/Backend/Totalnumretry.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Copyright (c) 2013-2016 Radial, Inc.
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.md.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @copyright   Copyright (c) 2013-2016 Radial, Inc. (http://www.radial.com/)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+class Radial_Payments_Model_Adminhtml_System_Config_Backend_Totalnumretry extends Mage_Core_Model_Config_Data
+{
+    public function _afterLoad()
+    {
+	$maxretries = Mage::helper('radial_core/payments/maxretries');
+
+        $pendingCreditMemoSize = Mage::getModel('sales/order_creditmemo')->getCollection()
+                        ->addFieldToFilter('state', Mage_Sales_Model_Order_Creditmemo::STATE_OPEN)->getSize();
+
+        $pendingInvoicesSize = Mage::getModel('sales/order_invoice')->getCollection()
+                                        ->addFieldToFilter('state', Mage_Sales_Model_Order_Invoice::STATE_OPEN)->getSize();
+
+        $objectCollectionSize = $pendingCreditMemoSize + $pendingInvoicesSize;	
+	
+	$publicDisplay = '# of Payment Messages Waiting For Transmission Retry: '. $objectCollectionSize;
+
+        $this->setValue($publicDisplay);
+        return $this;
+    }
+
+    /**
+     * Simply turn of data saving. This is a display-only field in admin dependent upon presence of
+     *  files in the filesystem, not any configuration data.
+     * @return self
+     * @codeCoverageIgnore Magento promises to not save anything if _dataSaveAllowed is false.
+     */
+    public function _beforeSave()
+    {
+        $this->_dataSaveAllowed = false;
+        return $this;
+    }
+}
+
---
+++ src/app/code/community/Radial/Payments/controllers/Adminhtml/AdminhtmlController.php
@@ -0,0 +1,121 @@
+<?php
+ 
+class Radial_Eb2cFraud_Adminhtml_AdminhtmlController extends Mage_Adminhtml_Controller_Action
+{
+    public function indexAction()
+    {
+	//Load Layout
+	$this->loadLayout();
+
+        //Render Layout
+        $this->renderLayout();
+    }
+
+    /**
+     * Reset Messages at Maximum Retries
+     */ 
+    public function messageResetAction()
+    {
+        Mage::getSingleton('adminhtml/session')->addSuccess("Successfully Reset Payments Messages at Maximum Transmission");
+	$maxretries = Mage::getStoreConfig('radial_core/payments/maxretries');
+
+	$pendingCreditMemo = Mage::getModel('sales/order_creditmemo')->getCollection()->setPageSize(100)
+                        ->addFieldToFilter('state', Mage_Sales_Model_Order_Creditmemo::STATE_OPEN)
+			->addFieldToFilter('delivery_status', $maxretries);
+
+        $pages = pendingCreditMemo->getLastPageNumber();
+        $currentPage = 1;
+
+        do
+        {
+                $pendingCreditMemo->setCurPage($currentPage);
+                $pendingCreditMemo->load();
+
+                foreach($pendingCreditMemo as $object)
+                {
+                        $object->setDeliveryStatus(0);
+                        $object->save();
+                }
+
+                $currentPage++;
+                $pendingCreditMemo->clear();
+        } while ($currentPage <= $pages);
+
+	$pendingInvoices = Mage::getModel('sales/order_invoice')->getCollection()->setPageSize(100)
+                                        ->addFieldToFilter('state', Mage_Sales_Model_Order_Invoice::STATE_OPEN)
+					->addFieldToFilter('delivery_status', $maxretries);
+
+	$pages = $pendingInvoices->getLastPageNumber();
+        $currentPage = 1;
+
+        do
+        {
+                $pendingInvoices->setCurPage($currentPage);
+                $pendingInvoices->load();
+
+                foreach($pendingInvoices as $object)
+                {
+                        $object->setDeliveryStatus(0);
+                        $object->save();
+                }
+
+                $currentPage++;
+                $pendingInvoices->clear();
+        } while ($currentPage <= $pages);
+
+        $this->_redirect('adminhtml/system_config/edit/section/radial_core');
+    }
+
+    /**
+     * Purge all messages in the retry queue
+     */ 
+    public function purgeRetryQueueAction()
+    {
+        Mage::getSingleton('adminhtml/session')->addSuccess("Successfully Purged Retry Payments Messages Queue");
+	$maxretries = Mage::getStoreConfig('radial_core/payments/maxretries');
+
+	 $pendingCreditMemo = Mage::getModel('sales/order_creditmemo')->getCollection()->setPageSize(100)
+                        ->addFieldToFilter('state', Mage_Sales_Model_Order_Creditmemo::STATE_OPEN);
+
+        $pages = pendingCreditMemo->getLastPageNumber();
+        $currentPage = 1;
+
+        do
+        {
+                $pendingCreditMemo->setCurPage($currentPage);
+                $pendingCreditMemo->load();
+
+                foreach($pendingCreditMemo as $object)
+                {
+                        $object->setDeliveryStatus(0);
+                        $object->save();
+                }
+
+                $currentPage++;
+                $pendingCreditMemo->clear();
+        } while ($currentPage <= $pages);
+
+        $pendingInvoices = Mage::getModel('sales/order_invoice')->getCollection()->setPageSize(100)
+                                        ->addFieldToFilter('state', Mage_Sales_Model_Order_Invoice::STATE_OPEN);
+
+        $pages = $pendingInvoices->getLastPageNumber();
+        $currentPage = 1;
+
+        do
+        {
+                $pendingInvoices->setCurPage($currentPage);
+                $pendingInvoices->load();
+
+                foreach($pendingInvoices as $object)
+                {
+                        $object->setDeliveryStatus(0);
+                        $object->save();
+                }
+
+                $currentPage++;
+                $pendingInvoices->clear();
+        } while ($currentPage <= $pages);
+
+        $this->_redirect('adminhtml/system_config/edit/section/radial_core');
+    }
+}
